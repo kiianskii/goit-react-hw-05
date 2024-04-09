07dc494f662077a055e03159ee31adc744ea96cb
kiianskii <ankiyanskiy@gmail.com>
2024-04-09 23:54:21 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import { lazy } from 'react'
 import Layout from './components/Layout/Layout'
 import HomePage from './pages/HomePage/HomePage'
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
+
 
 const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage'))
 const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage/MovieDetailsPage'))
 const MovieCast = lazy(() => import('./components/MovieCast/MovieCast'))
 const MovieReviews = lazy(() => import('./components/MovieReviews/MovieReviews'))
-const NotFoundPage = lazy(() => import('./pages/HomePage/HomePage'))
+// const NotFoundPage = lazy(() => import('./pages/HomePage/HomePage'))
 
 function App() {
   return (
@@ -21,7 +23,7 @@ function App() {
 					<Route path='reviews' element={<MovieReviews />} />
         </Route>
 			</Route>
-			<Route path='*' element={<NotFoundPage />} />
+			<Route path='*' element={<NotFoundPage/>} />
 		</Routes>
   )
 }
